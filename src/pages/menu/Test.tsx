cfc329e59c4e180762f31cd5f7c9abb775d108e9
李军鹏 <1315207983@qq.com>
2023-03-28 09:57:20 +0800

    feat: 测试组件

--- src/pages/menu/Test.tsx
+++ src/pages/menu/Test.tsx
@@ -1,11 +1,14 @@
-import { PageContainer } from '@ant-design/pro-components';
-import { Card } from 'antd';
 import React from 'react';
 const Test: React.FC = () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7];
   return (
-    <PageContainer>
-      <Card>123</Card>
-    </PageContainer>
+    <div className="content">
+      {arr.map((item) => (
+        <div key={item}>
+          <img src="https://imgcdn.dahebao.cn/20230314/20230314100358525585.png" alt="图片" />
+        </div>
+      ))}
+    </div>
   );
 };
 
--- src/pages/menu/charts/Test.tsx
+++ src/pages/menu/charts/Test.tsx
@@ -1,9 +1,28 @@
 // import { PageContainer } from '@ant-design/pro-components';
 // import { Card } from 'antd';
-import React from 'react';
-import './tset.css';
+import React, { useRef } from 'react';
+import './test.css';
 const Test: React.FC = () => {
-  return <div className="main">123rewrewr</div>;
+  const conRef = useRef<HTMLDivElement | null>(null);
+  const arr = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 22, 33, 44, 55];
+  const clickItem = () => {
+    console.log(123);
+    console.log(conRef);
+    if (conRef.current) conRef.current.scrollLeft += 100;
+  };
+  return (
+    <div className="content" onClick={clickItem} ref={conRef}>
+      {arr.map((item) => (
+        <div key={item}>
+          <img
+            className="con-img"
+            src="https://imgcdn.dahebao.cn/20230314/20230314100358525585.png"
+            alt="图片"
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Test;
